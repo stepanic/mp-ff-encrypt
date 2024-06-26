aa5049f8d2d277bdb956d89e6b26fa93a20af154
Matija Stepanic <stepanic.matija@gmail.com>
2024-06-26 17:02:54 +0200

    Local changes before sync

--- lib/custom_code/actions/encrypt_text_as_base64.dart
+++ lib/custom_code/actions/encrypt_text_as_base64.dart
@@ -7,14 +7,17 @@ import 'package:flutter/material.dart';
 // Begin custom action code
 // DO NOT REMOVE OR MODIFY THE CODE ABOVE!
 
+import 'package:encrypt/encrypt.dart' as encrypt;
+
 Future<String> encryptTextAsBase64(
   String plainText,
   String encryptionKey,
 ) async {
-  final key = Digest('SHA-256').process(utf8.encode(encryptionKey));
-  final iv = IV.fromLength(8);
-  final encrypter = Encrypter(Salsa20(key, iv));
-  final encryptedText = encrypter.encrypt(plainText);
+  final key = encrypt.Key.fromBase64(encryptionKey);
+  final iv = encrypt.IV.fromLength(8);
+  final encrypter = encrypt.Encrypter(encrypt.Salsa20(key));
+
+  final encryptedText = encrypter.encrypt(plainText, iv: iv);
 
   return encryptedText.base64;
 }
